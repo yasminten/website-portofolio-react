79578bae3516a1ec4d94a0243afb4bc1299aa0ec
yasminten <yasminten@outlook.com>
2021-11-22 17:18:14 +0700

    Container Mobile Hamburger

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -6,9 +6,11 @@ import { Link } from "react-scroll";
 import { useMediaQuery } from "react-responsive";
 import { deviceSize } from "../responsive";
 import { slide as Menu } from "react-burger-menu";
+import styles from "./menuStyles";
 
 const Container = styled.div`
-    ${tw`
+  width: 90%;
+  ${tw`
         flex
         h-20
         pl-6
@@ -26,13 +28,13 @@ const NavItems = tw.ul`
     list-none
     w-full
     h-auto
+    lg:w-auto
     lg:h-full
     flex
     lg:ml-20
     justify-center
     items-center
 `;
-
 
 const NavItem = tw.li`
     lg:mr-8
@@ -47,35 +49,38 @@ const NavItem = tw.li`
     lg:text-base
     transition-colors
     transition-duration[300ms]
-    hover:text-gray-300
+    hover:text-gray-200
     box-content
     mb-2
     lg:mb-0
 `;
 
-export function NavBar(){
-    
-    const isMobile = useMediaQuery({
-        maxWidth: deviceSize.mobile
-    })
+export function NavBar() {
+  const isMobile = useMediaQuery({ maxWidth: deviceSize.mobile });
 
-
-    const navItems = <NavItems>
-        <NavItem>
-            <Link to="Home">Home</Link>
-        </NavItem>
-        <NavItem>
-            <Link to="About">About</Link>
-        </NavItem>
+  const navItems = (
+    <NavItems>
+      <NavItem>
+        <Link to="Home">Home</Link>
+      </NavItem>
+      <NavItem>
+        <Link to="Explore">Explore</Link>
+      </NavItem>
+      <NavItem>
+        <Link to="TravelPlaces">Travel Places</Link>
+      </NavItem>
     </NavItems>
-    
-    return (
-        <Container>
-            <Logo />
-            {isMobile && <Menu right>
-                {navItems}
-            </Menu>}
-            {!isMobile && navItems}
-        </Container>
-    );
-};
+  );
+
+  return (
+    <Container>
+      <Logo />
+      {isMobile && (
+        <Menu right styles={styles}>
+          {navItems}
+        </Menu>
+      )}
+      {!isMobile && navItems}
+    </Container>
+  );
+}
